5dd45676740963d2712b45a0bf0c0fcede99e99a
mmarian <mihailmarian12@gmail.com>
2024-02-15 14:27:22 +0000

    site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "School of TikTok";
+export const SITE_DESCRIPTION =
+  "Your ultimate resource for expert tips and tricks to grow your TikTok account. Dive into bite-sized educational content designed to elevate your presence on the platform. From optimizing your content strategy to mastering trending challenges, we've got you covered. Join us on the journey to TikTok success!";
