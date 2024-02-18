8484f466825b4fa121f06b608416a67c0a23e0c3
mmarian <mihailmarian12@gmail.com>
2024-02-18 11:52:27 +0000

    site title and description tag

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "School of TikTok";
+export const SITE_TITLE =
+  "TikTok Advice: bite-sized tips to grow your TikTok audience";
 export const SITE_DESCRIPTION =
-  "Your ultimate resource for expert tips and tricks to grow your TikTok account. Dive into bite-sized educational content designed to elevate your presence on the platform. From optimizing your content strategy to mastering trending challenges, we've got you covered. Join us on the journey to TikTok success!";
+  "TikTok Advice: Quick tips in 2 mins or less for growing your account. Covering content strategy, viral video studies & more!";
