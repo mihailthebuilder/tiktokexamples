d825208217aaef9098f7889b730825b609b923e8
mmarian <mihailmarian12@gmail.com>
2024-02-20 17:46:29 +0000

    replaced old site references

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE =
-  "TikTok Advice: bite-sized tips to grow your TikTok audience";
+export const SITE_TITLE = "TikTok Examples: case studies for audience growth";
 export const SITE_DESCRIPTION =
-  "TikTok Advice: Quick tips in 2 mins or less for growing your account. Covering content strategy, viral video studies & more!";
+  "Learn from examples of TikTok posts or accounts that went viral. Curated to offer actionable strategies for audience growth that anyone can follow.";
