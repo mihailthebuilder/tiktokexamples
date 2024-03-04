c1db10c91809c37275e91aadebafd1f4c48c9dc3
mmarian <mihailmarian12@gmail.com>
2024-03-04 21:31:14 +0000

    exclude newsletter from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://tiktokexamples.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap({
+      filter: (page) => !page.includes("/newsletter/"),
+    }),
+    tailwind(),
+  ],
 });
